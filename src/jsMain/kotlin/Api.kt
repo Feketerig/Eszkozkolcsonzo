f4dde2e8bf0740b1e67f7bde72d4bcdadc31a705
ZachLevente <zach.levente@gmail.com>
2022-04-19 20:01:19 +0200

    add device add/delete functions to frontend

--- src/jsMain/kotlin/Api.kt
+++ src/jsMain/kotlin/Api.kt
@@ -2,6 +2,7 @@ import io.ktor.client.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import kotlinx.browser.window
 import model.Device
 
@@ -15,6 +16,13 @@ suspend fun getDeviceList(): List<Device>{
     return jsonClient.get(endpoint + Device.path)
 }
 
-suspend fun deleteDevice(id: Int): List<Device>{
+suspend fun deleteDevice(id: Int): List<Device>{ //TODO: if the return value is Unit changes are instant on the UI. Otherwise visible only after reload
     return jsonClient.delete(endpoint + Device.path + "/${id}")
+}
+
+suspend fun addDevice(device: Device): List<Device> { //TODO: if the return value is Unit changes are instant on the UI. Otherwise visible only after reload
+    return jsonClient.post(endpoint + Device.path){
+        contentType(ContentType.Application.Json)
+        body = device
+    }
 }
--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,35 +1,15 @@
+import components.device.DeviceList
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
-import model.Device
 import react.*
 import react.dom.html.ReactHTML
 
 private val scope = MainScope()
 
 val App = FC<Props> {
-    var deviceList by useState(emptyList<Device>())
-
-    useEffectOnce {
-        scope.launch {
-            deviceList = getDeviceList()
-        }
-    }
 
     ReactHTML.h1 {
         +"Eszközkölcsönző"
     }
-    ReactHTML.ul {
-        deviceList.forEach { item ->
-            ReactHTML.li {
-                key = item.toString()
-                +"${item.name} \t ${item.desc} \t ${item.available}"
-            }
-            onClick = {
-                scope.launch {
-                    deleteDevice(item.id)
-                    deviceList = getDeviceList()
-                }
-            }
-        }
-    }
+
+    DeviceList {}
 }
