92df5d42186bdc4e3861301a4999e99f8af89670
ZachLevente <zach.levente@gmail.com>
2022-05-22 21:46:24 +0200

    add user privilege query

--- src/commonMain/kotlin/utils/hash/Hash.kt
+++ src/commonMain/kotlin/utils/hash/Hash.kt
@@ -1,6 +1,9 @@
 package utils.hash
 
+import com.soywiz.krypto.encoding.Base64
 import com.soywiz.krypto.sha256
 import io.ktor.utils.io.core.*
 
 fun String.sha256(): String = this.toByteArray().sha256().hexLower
+
+fun String.base64Decode(): String = Base64.decode(this).decodeToString()
--- src/jsMain/kotlin/utils/browser/TokenStore.kt
+++ src/jsMain/kotlin/utils/browser/TokenStore.kt
@@ -15,7 +15,21 @@ object TokenStore {
         return window.sessionStorage.getItem(jwt_token) ?: ""
     }
 
-    fun get(key: String = jwt_token): String {
-        return window.sessionStorage.getItem(key) ?: ""
+
+    // TODO
+    /**
+     * Warning! This is not entirely safe, as users can modify session storage
+     * However, to get actual info from the server, the entire token has to be sent
+     * (This is currently only used for page headline button rendering
+     * Which by the way they can do manually anyway, by changing the url, so no effect there either)
+     */
+    fun getUserPrivilege(): User.Privilege? {
+        return try {
+            val token = window.sessionStorage.getItem(jwt_token)!!.base64Decode()
+            val priv = token.split("priv\":\"")[1].takeWhile { c -> c != '\"' }
+            User.Privilege.valueOf(priv)
+        } catch (e: Exception) {
+            null
+        }
     }
 }
